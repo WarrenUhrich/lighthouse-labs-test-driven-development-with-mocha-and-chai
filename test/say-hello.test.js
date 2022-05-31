b0fd69e8feaae54ad7a57734479e7f27e734434a
Warren <hello@warren.codes>
2022-05-31 13:23:05 -0600

    Install and use Mocha with node.js assert.

---
+++ test/say-hello.test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const {sayHello} = require('../say-hello');
+
+describe('tests for sayHello function', () => {
+    it('runs tests', () => {
+        const actual = 'test';
+        const expected = 'test';
+
+        assert.strictEqual(actual, expected);
+    });
+
+    it('returns "Hello there, Warren!', () => {
+        const actual = sayHello('Warren');
+        const expected = 'Hello there, Warren!';
+
+        assert.strictEqual(actual, expected);
+    });
+});
